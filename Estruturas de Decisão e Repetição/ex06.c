f5abe20d9472c13ff92cee35b7e0161b62bb45ce
Thays Ferreira <thaysferreiraua@gmail.com>
2023-01-26 19:11:13 -0300

    Update ex06.c

--- Estruturas de Decisão e Repetição/ex06.c
+++ Estruturas de Decisão e Repetição/ex06.c
@@ -1,39 +1,29 @@
 #include <stdio.h>
 
-//  Faça um Programa que receba a idade e a altura de várias pessoas. Calcule e imprima
+// Faça um Programa que receba a idade e a altura de várias pessoas. Calcule e imprima
 // a média das alturas das pessoas com mais de 50 anos. Para encerrar a entrada de
 // dados, digite idade < zero.
 
 int main()
 {
-    int idade[] = {};
-    float altura[] = {};
-    int i = 1;
-    float sumId = 0, sumAl = 0, size = 0;
+    int idade=0, size=0;
+    float altura, sumAlt, media;
     
-    printf("Preencha os campos e digite uma idade menor ou igual que zero para encerrar.\n");
-    
-    while(i > 0){
-        i = i - 1; // "consertando" o indice
-        
+    while(idade >= 0){
         printf("Digite sua idade: ");
-        scanf("%d", &idade[i]);
+        scanf("%d", &idade);
+
+        if(idade > 50){
+            printf("Digite sua altura: ");
+            scanf("%f", &altura);
         
-        if(idade[i] < 0) {
-            printf("Media de idade: %.2f\nMedia de altura: %.2f", sumId/size, sumAl/size);
-            i = 0;
-        } else {
-            
-        printf("Digite sua altura: ");
-        scanf("%f", &altura[i]);
-        
-        size = size + 1; // atualizando o tamanho dos vetores
-        sumId = sumId + idade[i]; // calculando a media das idades
-        sumAl = sumAl + altura[i]; // calculando a media das alturas
-        
-        i = i + 1;
+            size = size + 1;
+            sumAlt = sumAlt + altura;
+            media = sumAlt/size;
         }
     }
     
+    printf("Media de altura das pessoas com mais de 50 anos: %.2f", media);
+
     return 0;
 }
