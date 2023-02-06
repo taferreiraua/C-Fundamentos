4e22eb6f78f1c9991370bdc684de6f4d9f388f50
Thays Ferreira <thaysferreiraua@gmail.com>
2023-02-06 18:57:34 -0300

    Create ex04.c

---
+++ Vetores e Strings/ex04.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+
+int main()
+{
+    int A_vetor[] = {}, P_vetor[] = {};
+    int num = 1, size=0, aux;
+    
+    printf("Insira numeros inteiros positivos e digite zero para encerrar:\n");
+    while(num != 0){
+        scanf("%d", &num);
+        if(num != 0){
+            A_vetor[size] = num;
+            size = size + 1;
+        }
+    }
+
+    for(int a=0; a<size; a++){
+        for(int b=a; b<size; b++){
+            if(A_vetor[a]>A_vetor[b]){
+                aux = A_vetor[a];
+                A_vetor[a] = A_vetor[b];
+                A_vetor[b] = aux;
+            }
+        }
+    }
+    
+    for(int x=0; x<size; x++)
+        if((A_vetor[x] % 2) == 0)
+            P_vetor[j] = x;
+    
+    return 0;
+}
